0e1853ce8d77fca374896c86d12f2c2d42720891
SailFan <m17600576201@163.com>
2019-03-06 11:58:58 +0800

    first commit

---
+++ src/main/java/org/wsk/autoTestMock/common/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * vocy-1
+ * 上午9:47:23 
+*/
+/**
+ * @author liuwf
+ *  上午9:47:23
+ */
+package org.wsk.autoTestMock.common;
---
+++ src/main/java/org/wsk/autoTestMock/dao/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * vocy-1
+ * 上午9:47:02 
+*/
+/**
+ * @author liuwf
+ *  上午9:47:02
+ */
+package org.wsk.autoTestMock.dao;
