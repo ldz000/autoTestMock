4648fc62c8337f8fd529de6a0f66357e80d64506
SailFan <m17600576201@163.com>
2019-03-12 23:44:06 +0800

    完善mybatis，使用mybatis逆向工程生成mapper文件以及接口

--- src/main/java/org/wsk/autoTestMock/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package org.wsk.autoTestMock.dao;
-
-import org.wsk.autoTestMock.entity.Users;
-
-public interface UserMapper {
-	Users selectUser(int id);
-}
--- src/main/java/org/wsk/autoTestMock/testcase/MybatisTest.java
+++ src/main/java/org/wsk/autoTestMock/testcase/MybatisTest.java
@@ -1,17 +1,41 @@
 package org.wsk.autoTestMock.testcase;
 
+import static org.junit.Assert.*;
+
 import org.apache.ibatis.session.SqlSession;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.wsk.autoTestMock.common.MybatisUtil;
-import org.wsk.autoTestMock.dao.UserMapper;
-import org.wsk.autoTestMock.entity.Users;
+import org.wsk.autoTestMock.dao.UTestMapper;
+import org.wsk.autoTestMock.entity.UTest;
 
 public class MybatisTest {
-	@Test
-	public void test1(){
-		SqlSession session = MybatisUtil.getSession();
-		UserMapper mapper = session.getMapper(UserMapper.class);
-		Users user = mapper.selectUser(1);
-		System.out.println(user);
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		SqlSession session = MybatisUtil.getSession();
+		UTestMapper mapper = session.getMapper(UTestMapper.class);
+		UTest uTest = mapper.selectOneUser(1);
+		System.out.println(uTest);
+	}
+
 }
